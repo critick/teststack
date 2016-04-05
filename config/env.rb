3fad510c5930e18c1cc689e9213d65de1c4004a4
irfana02 <irfanahmad.cs@gmail.com>
2016-04-05 16:25:37 +0530

    poltergoist js to false

--- config/env.rb
+++ config/env.rb
@@ -128,7 +128,7 @@ case ENV['APP']
 
         Capybara.register_driver :poltergeist do |app|
 
-            options = { :js_errors => true,
+            options = { :js_errors => false,
                         :timeout => 100,
                         :debug => true,
                         :phantomjs_options => ['--ignore-ssl-errors=yes',
